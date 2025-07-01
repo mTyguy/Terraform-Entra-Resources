95eb177db153f113effbff0a45fde580b4644122
Tyler <119826083+mTyguy@users.noreply.github.com>
2025-07-01 18:08:44 -0400

    Update main.tf

--- conditionalAccess/namedLocations/main.tf
+++ conditionalAccess/namedLocations/main.tf
@@ -1,6 +1,6 @@
 # version 0.1  #
 
-####
+###
 
 provider "azuread" {
   tenant_id = var.tenant_id
@@ -13,6 +13,29 @@ resource "azuread_named_location" "UnitedStates" {
   country {
     countries_and_regions = [
       "US"
+    ]
+    country_lookup_method                 = "clientIpAddress"
+    include_unknown_countries_and_regions = false
+  }
+}
+
+resource "azuread_named_location" "Philippines" {
+  display_name = "Philippines Named Location"
+  country {
+    countries_and_regions = [
+      "PH"
+    ]
+    country_lookup_method                 = "clientIpAddress"
+    include_unknown_countries_and_regions = false
+  }
+}
+
+resource "azuread_named_location" "BritishIsles" {
+  display_name = "British Isles Named Location"
+  country {
+    countries_and_regions = [
+      "GB",
+      "IE"
     ]
     country_lookup_method                 = "clientIpAddress"
     include_unknown_countries_and_regions = false
