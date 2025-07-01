07e674ef9c88641b5c1dfd672037ea6dc7b6d84e
Tyler <119826083+mTyguy@users.noreply.github.com>
2025-07-01 18:20:46 -0400

    Update main.tf
    add Restrict_Philippines_Users

--- conditionalAccess/main.tf
+++ conditionalAccess/main.tf
@@ -106,3 +106,55 @@ resource "azuread_conditional_access_policy" "Passkey_Auth_Policy" {
     terms_of_use                      = []
   }
 }
+
+###
+
+#named location based CAs
+
+#Block Non-Philippines logins by Philippines users
+resource "azuread_conditional_access_policy" "Restrict_Philippines_Users" {
+  display_name = "Restrict Philippines Users (custom)"
+  state        = "enabled"
+
+  conditions {
+    client_app_types = [
+      "all",
+    ]
+    insider_risk_levels           = null
+    service_principal_risk_levels = []
+    sign_in_risk_levels           = []
+    user_risk_levels              = []
+
+    applications {
+      excluded_applications = []
+      included_applications = [
+        "All"
+      ]
+    }
+
+    locations {
+      excluded_locations = ["1a17a23b-992a-4aa2-93c1-524471daa902"]
+      included_locations = ["All"]
+    }
+
+    users {
+      excluded_groups = []
+      excluded_roles  = []
+      excluded_users = []
+      included_groups = [
+        "4f6851b7-5cef-41c7-ab00-2d9b1a876190", #Philippines Users Group
+      ]
+      included_roles = []
+      included_users = []
+    }
+  }
+
+  grant_controls {
+    authentication_strength_policy_id = null
+    built_in_controls = [
+      "block",
+    ]
+    operator     = "OR"
+    terms_of_use = []
+  }
+}
